7cff947036d060e2902e4c35ec1c111fd9ab55b4
Fernando Rodrigues Ferreira <fferreira12@gmail.com>
2018-09-07 18:56:47 -0300

    Novos metodos na classe Insumo e testes.

--- SIsConf/Model/Insumo.cs
+++ SIsConf/Model/Insumo.cs
@@ -8,10 +8,61 @@ namespace SisConf.Model
 {
     public class Insumo
     {
-
+        public int IdInsumo { get; set; }
+        public Marca Marca { get; set; }
+        public UnidadeMedida Unidade { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public UnidadeMedida Unidade { get; set; }
+        List<Aquisicao> Aquisicoes { get; }
 
+        public Insumo()
+        {
+            Aquisicoes = new List<Aquisicao>();
+        }
+
+
+        private class Aquisicao
+        {
+            public double Quantidade { get; set; }
+            public double PrecoUnitario { get; set; }
+        }
+
+        public void IncluirAquisicao(double quantidade, double precoUnitario)
+        {
+            Aquisicoes.Add(new Aquisicao()
+            {
+                Quantidade = quantidade,
+                PrecoUnitario = precoUnitario
+            });
+        }
+
+        public double CalcularPrecoMedio()
+        {
+            if (!PossuiAquisicoes())
+            {
+                return 0;
+            }
+            double ponderacao = Aquisicoes.Aggregate(0.0, (acc, x) => acc + x.Quantidade * x.PrecoUnitario);
+            return ponderacao / QuantidadeTotalAdquirida();
+        }
+
+        public double QuantidadeTotalAdquirida()
+        {
+            if (!PossuiAquisicoes())
+            {
+                return 0;
+            }
+            return Aquisicoes.Aggregate(0.0, (acc, x) => acc + x.Quantidade);
+        }
+
+        public bool PossuiAquisicoes()
+        {
+            return !(Aquisicoes == null || Aquisicoes.Count == 0);
+        }
+
+        public int QuantidadeDeAquisicoes()
+        {
+            return Aquisicoes.Count;
+        }
     }
 }
