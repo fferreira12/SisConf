2e08994530d31096c9d2f5813b03ca84654e9615
Fernando Rodrigues Ferreira <fferreira12@gmail.com>
2018-09-09 14:19:02 -0300

    Remove responsabilidades da classe Insumo e adiciona na Classe Estoque

--- SIsConf/Model/Estoque.cs
+++ SIsConf/Model/Estoque.cs
@@ -8,49 +8,113 @@ namespace SisConf.Model
 {
     public class Estoque
     {
-
+        public string NomeDoEstoque { get; set; }
         private Dictionary<Insumo, double> estoque = null;
+        private List<Aquisicao> aquisicoes = null;
 
 
-        public Estoque()
+        public Estoque(string nomeDoEstoque = null)
         {
+            NomeDoEstoque = nomeDoEstoque == null ? "" : nomeDoEstoque;
             estoque = new Dictionary<Insumo, double>();
+            aquisicoes = new List<Aquisicao>();
         }
 
         public Dictionary<Insumo, double> GetEstoque()
         {
+            AtualizarEstoque();
+
             return new Dictionary<Insumo, double>(estoque);
         }
 
-        public void Incluir(Insumo insumo, double quantidade)
+        private void AtualizarEstoque()
         {
-            if (estoque.ContainsKey(insumo))
+            estoque = new Dictionary<Insumo, double>();
+
+            foreach (Aquisicao aquisicao in aquisicoes)
             {
-                estoque[insumo] += quantidade;
-            } else
-            {
-                estoque[insumo] = quantidade;
+                if (estoque.ContainsKey(aquisicao.Insumo))
+                {
+                    estoque[aquisicao.Insumo] += aquisicao.Quantidade;
+                }
+                else
+                {
+                    estoque[aquisicao.Insumo] = aquisicao.Quantidade;
+                }
             }
         }
 
-        public void Retirar(Insumo insumo, double quantidade)
+        public void IncluirAquisicao(params Aquisicao[] aquisicoes)
         {
-            if (estoque.ContainsKey(insumo))
+            foreach (Aquisicao aquisicao in aquisicoes)
             {
-                estoque[insumo] -= quantidade;
-            }
-            else
-            {
-                estoque[insumo] = 0;
+                this.aquisicoes.Add(aquisicao);
             }
         }
 
-        public double GetQuantidade(Insumo insumo)
+        public void RemoverAquisicao(Aquisicao aquisicao)
         {
+            aquisicoes.Remove(aquisicao);
+        }
+
+        public double ObterQuantidade(Insumo insumo)
+        {
+            AtualizarEstoque();
             return estoque[insumo];
         }
 
+        public double ValorTotalDoEstoque()
+        {
+            double valorTotal = 0;
+            foreach (Aquisicao aquisicao in aquisicoes)
+            {
+                valorTotal += aquisicao.Quantidade * aquisicao.PrecoUnitario;
+            }
+            return valorTotal;
+        }
 
+        public double CalcularPrecoMedio(Insumo insumo = null)
+        {
+            if(insumo == null)
+            {
+                return 0;
+            }
+
+            double ponderacao = 0;
+
+            aquisicoes.ForEach((aquisicao) =>
+            {
+                if(insumo != null && aquisicao.Insumo == insumo)
+                {
+                    ponderacao += aquisicao.Quantidade * aquisicao.PrecoUnitario;
+                }
+            });
+
+            return ponderacao / ObterQuantidade(insumo);
+
+        }
+
+        public bool PossuiAquisicoes(Insumo insumo)
+        {
+            return aquisicoes != null && aquisicoes.FirstOrDefault((aquisicao) => aquisicao.Insumo == insumo) != null;
+        }
+
+        public int QuantidadeDeAquisicoes(Insumo insumo)
+        {
+            if(insumo == null)
+            {
+                return 0;
+            }
+            int quantidade = 0;
+            foreach (Aquisicao aquisicao in aquisicoes)
+            {
+                if(aquisicao.Insumo == insumo)
+                {
+                    quantidade++;
+                }
+            }
+            return quantidade;
+        }
 
     }
 }
--- SIsConf/Model/Insumo.cs
+++ SIsConf/Model/Insumo.cs
@@ -8,61 +8,30 @@ namespace SisConf.Model
 {
     public class Insumo
     {
+        private Marca marca = null;
         public int IdInsumo { get; set; }
-        public Marca Marca { get; set; }
+        public Marca Marca
+        {
+            get {
+                return marca;
+            }
+            set {
+                if(marca != null)
+                {
+                    value.IncluirInsumo(this);
+                }
+                marca = value;
+            }
+        }
         public UnidadeMedida Unidade { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        List<Aquisicao> Aquisicoes { get; }
 
-        public Insumo()
+        public Insumo(string nomeDoInsumo = null, Marca marca = null)
         {
-            Aquisicoes = new List<Aquisicao>();
+            Nome = nomeDoInsumo;
+            Marca = marca == null ? new Marca() : marca;
         }
 
-
-        private class Aquisicao
-        {
-            public double Quantidade { get; set; }
-            public double PrecoUnitario { get; set; }
-        }
-
-        public void IncluirAquisicao(double quantidade, double precoUnitario)
-        {
-            Aquisicoes.Add(new Aquisicao()
-            {
-                Quantidade = quantidade,
-                PrecoUnitario = precoUnitario
-            });
-        }
-
-        public double CalcularPrecoMedio()
-        {
-            if (!PossuiAquisicoes())
-            {
-                return 0;
-            }
-            double ponderacao = Aquisicoes.Aggregate(0.0, (acc, x) => acc + x.Quantidade * x.PrecoUnitario);
-            return ponderacao / QuantidadeTotalAdquirida();
-        }
-
-        public double QuantidadeTotalAdquirida()
-        {
-            if (!PossuiAquisicoes())
-            {
-                return 0;
-            }
-            return Aquisicoes.Aggregate(0.0, (acc, x) => acc + x.Quantidade);
-        }
-
-        public bool PossuiAquisicoes()
-        {
-            return !(Aquisicoes == null || Aquisicoes.Count == 0);
-        }
-
-        public int QuantidadeDeAquisicoes()
-        {
-            return Aquisicoes.Count;
-        }
     }
 }
