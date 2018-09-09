40d4ea9f7a9740e1e4c8072ec63e296d6cce311f
Fernando Rodrigues Ferreira <fferreira12@gmail.com>
2018-09-09 14:18:03 -0300

    Adiciona classe Aquisicao

---
+++ SIsConf/Model/Aquisicao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisConf.Model
+{
+    public class Aquisicao
+    {
+
+        public Insumo Insumo { get; set; }
+        public double Quantidade { get; set; }
+        public double PrecoUnitario { get; set; }
+        public DateTime Data { get; set; }
+
+        public Aquisicao(){}
+
+        public Aquisicao(Insumo insumo, double quantidade, double precoUnitario)
+        {
+            Insumo = insumo;
+            Quantidade = quantidade;
+            PrecoUnitario = precoUnitario;
+        }
+
+    }
+}
