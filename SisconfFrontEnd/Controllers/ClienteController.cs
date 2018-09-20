6d9a25da2560d9dcd86be900813d244f231e2c12
Fernando Rodrigues Ferreira <fferreira12@hotmail.com>
2018-09-20 17:47:30 -0300

    Adiciona páginas para Cliente, ainda em teste

---
+++ SisconfFrontEnd/Controllers/ClienteController.cs
@@ -0,0 +1,28 @@
+﻿using SisConf.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SisConf.Model;
+
+namespace SisconfFrontEnd.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Clientes()
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Fernando";
+
+            Cliente c2 = new Cliente()
+            {
+                Nome = "Jane"
+            };
+            return View(new Cliente[] { cliente, c2 });
+        }
+
+        public ActionResult Adicionar() => View();
+    }
+}
