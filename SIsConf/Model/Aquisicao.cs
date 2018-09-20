3555e6607cb30a8375cc5e4f45397f3df5cf94e6
Fernando Rodrigues Ferreira <fferreira12@hotmail.com>
2018-09-20 15:56:15 -0300

    Adiciona interface IIdentificadoNUmericamente para entidades do Core

--- SIsConf/Model/Aquisicao.cs
+++ SIsConf/Model/Aquisicao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SisConf.Model.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SisConf.Model
 {
-    public class Aquisicao
+    public class Aquisicao : IIdentificadoNumericamente
     {
         public int Id { get; set; }
         public Insumo Insumo { get; set; }
